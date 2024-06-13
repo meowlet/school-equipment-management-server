fa4feaa0acec462ca1ea1ae5430b5e09fb46c0bf
meowlet <ngoclink13@gmmail.com>
2024-06-13 18:45:57 +0700

    Feature: Add corresponding models

---
+++ src/model/EquipmentType.ts
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose";
+import { IEquipmentType } from "../util/Entity";
+
+const equipmentTypeSchema = new Schema<IEquipmentType>(
+  {
+    typeCode: { type: String, required: true },
+    typeName: { type: String, required: true },
+    description: { type: String },
+  },
+  {
+    timestamps: true,
+  },
+);
+ 
+export const EquipmentType = model<IEquipmentType>("EquipmentType", equipmentTypeSchema);
