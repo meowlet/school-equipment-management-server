0b4c4dc8bdaeddaa9ebf2748f2beb30c85c2a241
meowlet <ngoclink13@gmmail.com>
2024-06-13 18:45:16 +0700

    Feature: Add the Supplier and EquipmentType interfaces

--- src/util/Entity.ts
+++ src/util/Entity.ts
@@ -1,4 +1,4 @@
-import { Schema } from "mongoose";
+import { Types } from "mongoose";
 
 // Interface for Users
 export interface IUser {
@@ -6,7 +6,7 @@ export interface IUser {
   passwordHash: string;
   fullName?: string;
   email?: string;
-  roleId: Schema.Types.ObjectId;
+  roleId: Types.ObjectId;
 }
 
 // Interface for Equipment
@@ -14,20 +14,39 @@ export interface IEquipment {
   equipmentName: string;
   description?: string;
   status: string;
-  currentLocationId: Schema.Types.ObjectId;
+  currentLocationId: Types.ObjectId;
+  supplier: Types.ObjectId;
+  type: Types.ObjectId;
+}
+
+// Interface for Supplier
+export interface ISupplier {
+  name: string;              
+  email: string;           
+  phoneNumber: string;    
+  address: string;       
+  website?: string;     
+  notes?: string;      
+}
+
+//Interface for EquipmentType 
+export interface IEquipmentType {
+  typeCode: string;
+  typeName: string;
+  description?: string
 }
 
 // Interface for LoanHistory
 export interface ILoanHistory {
-  userId: Schema.Types.ObjectId;
-  equipmentId: Schema.Types.ObjectId;
+  userId: Types.ObjectId;
+  equipmentId: Types.ObjectId;
   loanDate: Date;
   returnDate?: Date;
 }
 
 // Interface for Maintenance
 export interface IMaintenance {
-  equipmentId: Schema.Types.ObjectId;
+  equipmentId: Types.ObjectId;
   maintenanceDate: Date;
   notes?: string;
 }
