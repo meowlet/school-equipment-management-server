0b64955f7c64a1295fbee5e1533f308f936ae98a
meowlet <ngoclink13@gmmail.com>
2024-06-14 12:43:16 +0700

    Chore: Update the wrapper

--- src/Application.ts
+++ src/Application.ts
@@ -1,5 +1,8 @@
 import Elysia from "elysia";
 import { GetEquipmentController } from "./controller/equipment/GetEquipmentController";
+import { CreateEquipmentController } from "./controller/equipment/CreateEquipmentController";
 
 export const Application = (app: Elysia) =>
-  app.group("/equipment", (app) => app.use(GetEquipmentController));
+  app.group("/equipment", (app) =>
+    app.use(GetEquipmentController).use(CreateEquipmentController),
+  );
--- src/Index.ts
+++ src/Index.ts
@@ -1,9 +1,8 @@
 import { Elysia } from "elysia";
 import { Application } from "./Application";
+import "./database/MongoDBSetup";
 
-const app = new Elysia()
-  .use(Application)
-  .listen(3000);
+const app = new Elysia().use(Application).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
