9e93776f650cea621c36428ce8e60440014a1719
meowlet <ngoclink13@gmail.com>
2024-06-14 17:30:49 +0700

    Feature: Add error handler and js object parser

--- src/Index.ts
+++ src/Index.ts
@@ -1,8 +1,14 @@
 import { Elysia } from "elysia";
 import { Application } from "./Application";
 import "./database/MongoDBSetup";
+import { getErrorMessage } from "./util/Error";
 
-const app = new Elysia().use(Application).listen(3000);
+const app = new Elysia()
+  .onError(({ error }) => {
+    return new Response(getErrorMessage(error));
+  })
+  .use(Application)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
