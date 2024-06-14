93b939f0c584b2515217d365681b7e23ed182c7e
meowlet <ngoclink13@gmmail.com>
2024-06-15 00:52:18 +0700

    Feature: Finish the error handler

--- src/Index.ts
+++ src/Index.ts
@@ -1,14 +1,10 @@
 import { Elysia } from "elysia";
 import { Application } from "./Application";
 import "./database/MongoDBSetup";
-import { getErrorMessage } from "./util/Error";
+import { ErrorPlugin } from "./plugin/ErrorPlugin";
 
-const app = new Elysia()
-  .onError(({ error }) => {
-    return new Response(getErrorMessage(error));
-  })
-  .use(Application)
-  .listen(3000);
+const app = new Elysia();
+app.use(ErrorPlugin).use(Application).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
