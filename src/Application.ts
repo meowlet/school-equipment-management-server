9cc2eea0c13e555f9fb52ca363fa0e88ade6846b
meowlet <ngoclink13@gmmail.com>
2024-06-14 17:30:49 +0700

    Feature: Add error handler and js object parser

--- src/Application.ts
+++ src/Application.ts
@@ -1,8 +1,12 @@
 import Elysia from "elysia";
 import { GetEquipmentController } from "./controller/equipment/GetEquipmentController";
 import { CreateEquipmentController } from "./controller/equipment/CreateEquipmentController";
+import { SignInController } from "./controller/auth/SignInController";
+import { SignUpController } from "./controller/auth/SignUpController";
 
 export const Application = (app: Elysia) =>
-  app.group("/equipment", (app) =>
-    app.use(GetEquipmentController).use(CreateEquipmentController),
-  );
+  app
+    .group("/equipment", (app) =>
+      app.use(GetEquipmentController).use(CreateEquipmentController),
+    )
+    .group("/auth", (app) => app.use(SignInController).use(SignUpController));
--- src/util/Entity.ts
+++ src/util/Entity.ts
@@ -3,17 +3,17 @@ import { Resource, Action } from "./Enum";
 
 // Interface for Users
 export interface IUser {
-  username: string;
+  userName: string;
   passwordHash: string;
-  fullName?: string;
-  email?: string;
+  fullName: string;
+  email: string;
   roleId: Types.ObjectId;
 }
 
 // Interface for Equipment
 export interface IEquipment {
   equipmentName: string;
-  description?: string;
+  description: string;
   status: string;
   currentLocationId: Types.ObjectId;
   supplier: Types.ObjectId;
@@ -27,15 +27,15 @@ export interface ISupplier {
   email: string;
   phoneNumber: string;
   address: string;
-  website?: string;
-  notes?: string;
+  website: string;
+  notes: string;
 }
 
 //Interface for EquipmentType
 export interface IEquipmentType {
   typeCode: string;
   typeName: string;
-  description?: string;
+  description: string;
 }
 
 // Interface for LoanHistory
@@ -43,25 +43,25 @@ export interface ILoanHistory {
   userId: Types.ObjectId;
   equipmentId: Types.ObjectId;
   loanDate: Date;
-  returnDate?: Date;
+  returnDate: Date;
 }
 
 // Interface for Maintenance
 export interface IMaintenance {
   equipmentId: Types.ObjectId;
   maintenanceDate: Date;
-  notes?: string;
+  notes: string;
 }
 
 // Interface for Locations
 export interface ILocation {
   locationName: string;
-  description?: string;
+  description: string;
 }
 
 // Interface for Roles
 export interface IRole {
   name: string;
-  description?: string;
+  description: string;
   permissions: { resource: Resource; actions: Action[] }[];
 }
